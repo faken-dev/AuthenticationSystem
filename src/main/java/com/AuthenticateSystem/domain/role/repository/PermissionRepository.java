64efc9f564de555c21589c0ed5066261fa97e7e8
Faken <faken.dev@gmail.com>
2026-03-18 14:38:03 +0700

    feat(repository): add permission, role and user repositories

---
+++ src/main/java/com/AuthenticateSystem/domain/role/repository/PermissionRepository.java
@@ -0,0 +1,19 @@
+package com.AuthenticateSystem.domain.role.repository;
+
+import com.AuthenticateSystem.domain.role.entity.Permission;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, UUID> {
+
+    Optional<Permission> findByName(String name);
+
+    boolean existsByName(String name);
+
+    Set<Permission> findAllByNameIn(Set<String> names);
+}
